90ccc4459b1a3db64e1353445545f82a2721a07a
Piekielniok <piekielniok@wp.pl>
2025-01-14 23:27:52 +0100

    Style login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,44 @@
 import { useState } from 'react';
 
 function Login() {
+  const [selectedForm, setSelectedForm] = useState('login');
   const [loginData, setLoginData] = useState({
     login: "",
-    password: ""
+    password: "",
+    email: "",
+    repeatPassword: ""
   });
 
   return (
-    <div>
-      <h1>Logowanie</h1>
-      <label htmlFor="loginInput">Login: </label>
-      <input value={loginData.login} type="text" name="login" id="loginInput" onChange={e => setLoginData({ ...loginData, login: e.target.value})}/>
-      <label htmlFor="password">Hasło: </label>
-      <input value={loginData.password} type="password" name="password" id="passwordInput" onChange={e => setLoginData({ ...loginData, password: e.target.value})}/>
+    <div className="login-container">
+      <div className="login-switch-container">
+        <button className={selectedForm === 'login' ? 'selected' : ''} onClick={e => setSelectedForm('login')}>Logowanie</button>
+        <button className={selectedForm === 'register' ? 'selected' : ''} onClick={e => setSelectedForm('register')}>Rejestracja</button>
+      </div>
+      {selectedForm === 'login' ? (
+        <div className="login-form-container">
+          <h1>Logowanie</h1>
+          <label htmlFor="loginInput">Login: </label>
+          <input value={loginData.login} type="text" name="login" id="loginInput" onChange={e => setLoginData(prevState => { return {...prevState, login: e.target.value}})} />
+          <label htmlFor="password">Hasło: </label>
+          <input value={loginData.password} type="password" name="password" id="passwordInput" onChange={e => setLoginData(prevState => { return {...prevState, password: e.target.value}})} />
+          <button className={loginData.login !== '' && loginData.password !== '' ? 'form-filled' : ''}>Zaloguj się</button>
+        </div>
+      ) : ''}
+      {selectedForm === 'register' ? (
+        <div className="register-form-container">
+          <h1>Rejestracja</h1>
+          <label htmlFor="loginInput">Login: </label>
+          <input value={loginData.login} type="text" name="login" id="loginInput" onChange={e => setLoginData(prevState => { return {...prevState, login: e.target.value}})} />
+          <label htmlFor="emailInput">Email: </label>
+          <input value={loginData.email} type="email" name="email" id="emailInput" onChange={e => setLoginData(prevState => { return {...prevState, email: e.target.value}})} />
+          <label htmlFor="passwordInput">Hasło: </label>
+          <input value={loginData.password} type="password" name="password" id="passwordInput" onChange={e => setLoginData(prevState => { return {...prevState, password: e.target.value}})} />
+          <label htmlFor="repeatPasswordInput">Powtórz hasło: </label>
+          <input value={loginData.repeatPassword} type="password" name="repeat_password" id="repeatPasswordInput" onChange={e => setLoginData(prevState => { return {...prevState, repeatPassword: e.target.value}})} />
+          <button className={loginData.login !== '' && loginData.password !== '' && loginData.repeatPassword !== '' ? 'form-filled' : ''}>Zarejestruj się</button>
+        </div>
+      ) : ''}
     </div>
   );
 }
